7b53864996cfa6954be0beee85b7c3c297c40c80
ranaba665-afk <ranaba665@gmail.com>
2026-09-04 07:00:27 +0530

    Update tournamentRoutes.js

--- routes/tournamentRoutes.js
+++ routes/tournamentRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { protect } = require("../middleware/auth");
 const { listTournaments, getTournamentById } = require("../controllers/tournamentController");
 const { bookSlot } = require("../controllers/bookingController");
+const { bookWithWallet } = require("../controllers/walletController");
 
 // Public
 router.get("/", listTournaments);
@@ -10,5 +11,6 @@ router.get("/:id", getTournamentById);
 
 // Requires login
 router.post("/:tournamentId/book", protect, bookSlot);
+router.post("/:tournamentId/book-with-wallet", protect, bookWithWallet);
 
 module.exports = router;
